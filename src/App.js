6075e6ce63900870245a910f9746359e70116b7c
caarlosdamian <dapecarlos@gmail.com>
2021-07-22 18:41:32 -0500

    add singout button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 import Singin from "./components/Singin";
 import Singup from "./components/Singup";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      const user = {
+        uuid: userAuth?.uid,
+        email: userAuth?.email,
+      };
+      if (userAuth) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={Singin} />
+          {user ? (
+            <Route path="/" exact component={Home} />
+          ) : (
+            <Route path="/" exact component={Singin} />
+          )}
+
           <Route path="/singup" exact component={Singup} />
         </Switch>
       </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { auth } from '../firebase'
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome Home</h1>
+            <button className="waves-effect red btn" onClick={()=>auth.signOut()}>Sing out</button>
+
         </div>
     )
 }
